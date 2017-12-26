c8778efa73150947d14e0bd556078e44b586c9ed
kaven feng Application Admin <fengkaadmin@ads.autodesk.com>
2017-12-26 00:06:43 -0800

    add /health api

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , health = require('./routes/health');
 
 var app = express();
 
@@ -15,6 +17,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(bodyParser.json());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,7 +32,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
+app.get('/health', health.health);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/health.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+exports.health = function(req, res){
+  res.json({ message: 'ok' });   
+};
